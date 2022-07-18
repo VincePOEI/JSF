0bab2a683dcc3dddda2e695fa2632a0562b7babe
Vincent <vanth.vincent@gmail.com>
2022-07-18 14:31:52 +0200

    NavConidtionnelle

--- src/main/java/NavigationController.java
+++ src/main/java/NavigationController.java
@@ -1,16 +1,37 @@
 
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean(name="navigationController")
 public class NavigationController {
-
+    
+    @ManagedProperty (value = "#{param.pageId}")
+    private String pageId;
     
     public NavigationController() {
     }
     
+    
+    
     public String moveToHelloPage(){
         return "hello";
+    }
+    
+    public String goToPage(){
+        if(pageId != null){
+            return pageId;
+        }
+        
+        return "home";
+    }
+    
+     public String getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
     }
     
 }
