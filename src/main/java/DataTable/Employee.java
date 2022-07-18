3d5a23f2f9687749474ebde9d6050008fc69389c
Vincent <vanth.vincent@gmail.com>
2022-07-18 16:48:47 +0200

    dataTable

---
+++ src/main/java/DataTable/Employee.java
@@ -0,0 +1,52 @@
+
+package DataTable;
+
+
+public class Employee {
+
+    private String name;
+    private String department;
+    private Integer age;
+    private Integer salary;
+    
+    
+    public Employee(String name, String department,Integer age, Integer salary){
+        this.name = name;
+        this.department = department;
+        this.age = age;
+        this.salary = salary;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+    
+}
