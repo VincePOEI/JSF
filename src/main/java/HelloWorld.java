4cec374dc4881d44eea829c02adabd50ce04c97c
Vincent <vanth.vincent@gmail.com>
2022-07-18 12:36:17 +0200

    HelloWorld et message

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
- */
+
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
